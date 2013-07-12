c88b31496bfa8ef017921fef7bb49d99ca75f594
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2013-07-12 20:57:50 +0400

    acer added

---
+++ acer3830.el
@@ -0,0 +1,6 @@
+;; ANB changes
+(set-face-attribute 'default nil :height 120)
+(add-to-list 'default-frame-alist '(height . 35))
+(add-to-list 'default-frame-alist '(width . 90))
+(global-linum-mode 1)
+
